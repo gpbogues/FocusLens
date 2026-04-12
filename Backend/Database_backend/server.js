e6dfe853a37dfc724cb5774339e7d0add1d65dc9
Shwguey <sebastian.guuuuu@gmail.com>
2026-04-12 17:38:53 -0500

    update db txt

--- Backend/Database_backend/server.js
+++ Backend/Database_backend/server.js
@@ -74,7 +74,6 @@ db.exec(`
     verified      INTEGER NOT NULL DEFAULT 0,
     created_at    TEXT NOT NULL DEFAULT (datetime('now')),
     avatarUrl     TEXT NULL DEFAULT NULL,
-    isDarkMode    INTEGER,
     cameraEnabled INTEGER,
     micEnabled    INTEGER,
     avatarId      TEXT,
